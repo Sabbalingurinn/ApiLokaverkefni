3d31bc61b4fbe56c094adc94a8941503ff23681a
Sabbalingurinn <s.b.snorrason@gmail.com>
2025-04-14 16:54:38 +0000

    komið vonandi

--- src/lib/corsMiddleware.ts
+++ src/lib/corsMiddleware.ts
@@ -1,6 +1,7 @@
 import type { MiddlewareHandler } from 'hono';
 
 export const corsMiddleware: MiddlewareHandler = async (c, next) => {
+  console.log('🔥 CORS middleware virkt!');
   c.res.headers.set('Access-Control-Allow-Origin', '*'); // ← Prófaðu '*' til að tryggja virkni
   c.res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   c.res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
