4ac7ed312cbea72629b083c5edf40ccf71c24a04
Sabbalingurinn <s.b.snorrason@gmail.com>
2025-04-14 19:25:44 +0000

    pl

--- src/lib/corsMiddleware.ts
+++ src/lib/corsMiddleware.ts
@@ -1,14 +1,21 @@
 import type { MiddlewareHandler } from 'hono';
 
 export const corsMiddleware: MiddlewareHandler = async (c, next) => {
-  console.log('🔥 CORS middleware virkt!');
-  c.res.headers.set('Access-Control-Allow-Origin', '*'); // ← Prófaðu '*' til að tryggja virkni
-  c.res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  c.res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  const headers = new Headers();
+  headers.set('Access-Control-Allow-Origin', '*');
+  headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (c.req.method === 'OPTIONS') {
-    return new Response(null, { status: 204 });
+    return new Response(null, {
+      status: 204,
+      headers,
+    });
   }
+
+  c.res.headers.set('Access-Control-Allow-Origin', '*');
+  c.res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  c.res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   await next();
 };
