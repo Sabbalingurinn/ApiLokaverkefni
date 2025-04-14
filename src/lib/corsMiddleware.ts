7dbe4c2b7eed7ef16d43673968278bda7786815c
Sabbalingurinn <s.b.snorrason@gmail.com>
2025-04-14 21:28:55 +0000

    pls

--- src/lib/corsMiddleware.ts
+++ src/lib/corsMiddleware.ts
@@ -3,7 +3,7 @@ import type { MiddlewareHandler } from 'hono';
 export const corsMiddleware: MiddlewareHandler = async (c, next) => {
   const headers = new Headers();
   headers.set('Access-Control-Allow-Origin', '*');
-  headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
   headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (c.req.method === 'OPTIONS') {
@@ -14,7 +14,7 @@ export const corsMiddleware: MiddlewareHandler = async (c, next) => {
   }
 
   c.res.headers.set('Access-Control-Allow-Origin', '*');
-  c.res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  c.res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
   c.res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   await next();
